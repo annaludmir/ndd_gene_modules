6e20b4c2f4bffe1c5f354f9813e0e2c867fd29d3
annaludmir <annadenisenko13@gmail.com>
2026-04-08 01:59:01 +0300

    Change gene expression summary running file to work with corrected paths

--- running_scripts/python_gene_expression_summary.sh
+++ running_scripts/python_gene_expression_summary.sh
@@ -26,14 +26,14 @@ module load mamba/mamba-1.5.8
 mamba activate /miridan-data/annaludmir/conda-envs/jupyter-scanpy_new
 cd /miridan-data/annaludmir/ndd_gene_modules
 
-INPUT_FOLDER="results/enrichment_results/ID/"
-TOP_N=25
+GSEA_SUMMARY_FILE="/miridan-data/annaludmir/ndd_gene_modules/results/enrichment_results/Microcephaly All Data (Without Week 5)_threshold_1_20260323/data/enrichment_results/GSEA/GSEA_final_summary.csv"
+LEADING_GENE_SUMMARY_FILE="/miridan-data/annaludmir/ndd_gene_modules/results/enrichment_results/microcephaly/Microcephaly Cell Phase_threshold_1_20260222/data/enrichment_results/GSEA/GSEA_final_summary.csv"
 
 mamba run -p /miridan-data/annaludmir/conda-envs/jupyter-scanpy_new python modules/gene_expression_summary.py \
   --h5ad-path data/human_dev.h5ad \
-  --gsea-summary-file /miridan-data/annaludmir/ndd_gene_modules/results/enrichment_results/Microcephaly All Data (Without Week 5)_threshold_1_20260323/data/enrichment_results/GSEA/GSEA_final_summary.csv \
+  --gsea-summary-file "$GSEA_SUMMARY_FILE" \
   --condition Forebrain \
-  --leading-gene-summary-file /miridan-data/annaludmir/ndd_gene_modules/results/enrichment_results/microcephaly/Microcephaly Cell Phase_threshold_1_20260222/data/enrichment_results/GSEA/GSEA_final_summary.csv \
+  --leading-gene-summary-file "$LEADING_GENE_SUMMARY_FILE" \
   --leading-gene-conditions S G2M G1 PostM Non-cycling \
   --subfolder-name forebrain_gene_expression_summary
 
